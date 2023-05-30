8106f332bab580a22ff07c0e795dad4961bf2382
Vasyl94 <basyl94@icloud.com>
2023-05-30 23:06:33 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from "notiflix";
-import { getImage } from "./getImages";
+import { getImage } from "./getImages.js";
 
 let pagecount = 1;
 let hits = 0;
